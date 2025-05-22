70b9d22e21669609c98b33f7c9524b2faf4e2524
DeporS <depores1337@gmail.com>
2025-05-22 10:45:31 +0200

    ASP.NET MVC

---
+++ MvcNews/MvcNews_2/Data/NewsDbContent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNews_2.Models;
+
+namespace MvcNews_2.Data
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) :
+        base(options)
+        { }
+        public DbSet<NewsItem> News { get; set; } = null!;
+    }
+}
---
+++ MvcNews/MvcNews_2/Models/NewsItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace MvcNews_2.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        [Required(ErrorMessage = "Treść newsa jest wymagana.")]
+        [StringLength(140, MinimumLength = 5, ErrorMessage = "Treść newsa musi mieć od 5 do 140 znaków.")]
+        public string Text { get; set; }
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
